0e0fdf4a43d1aa7ca0d300d8caee83db4ca4e0c3
Skull1991 <skull.shanto10@gmail.com>
2022-07-23 10:15:47 +0545

    Function and path added for search function

--- product/urls.py
+++ product/urls.py
@@ -7,13 +7,16 @@ urlpatterns = [
     path("xbox",views.xbox),
     path("nintendo",views.nintendo),
     path("details/<int:id>",views.details),
-     path('cart/add/<int:id>/', views.cart_add, name='cart_add'),
+    path('cart/add/<int:id>/', views.cart_add, name='cart_add'),
     path('cart/item_clear/<int:id>/', views.item_clear, name='item_clear'),
     path('cart/item_increment/<int:id>/',
          views.item_increment, name='item_increment'),
     path('cart/item_decrement/<int:id>/',
          views.item_decrement, name='item_decrement'),
     path('cart/cart_clear/', views.cart_clear, name='cart_clear'),
-    path('cart/cart-detail/',views.cart_detail,name='cart_detail')
+    path('cart/cart-detail/',views.cart_detail,name='cart_detail'),
+    path("order",views.order),
+    path("checkout",views.checkout),
+    path('search_venues',views.search_venues,name='search-venues'),
 
 ]
--- product/views.py
+++ product/views.py
@@ -2,13 +2,23 @@
 
 # Create your views here.
 from django.shortcuts import redirect, render
-
+from orders.models import Order
+from django.contrib.auth.models import User
 
 from product.models import Product
 from product.forms import ProductForm
 # Create your views here.
 from cart.cart import Cart
 from django.contrib.auth.decorators import login_required
+
+def search_venues(request):
+    if request.method== "POST":
+        searched = request.POST['searched']
+        products= Product.objects.filter(name__icontains=searched)
+        return render(request,"search.html",{'products':products,'searched':searched})
+    else:
+        return render(request,"search.html",{})
+
 
 def home(request):
     console=Product.objects.filter(category='console')[0:3]
@@ -44,7 +54,7 @@ def cart_add(request, id):
     return redirect("/")
 
 
-
+@login_required(login_url="/user/login")
 def item_clear(request, id):
     cart = Cart(request)
     product = Product.objects.get(id=id)
@@ -52,7 +62,7 @@ def item_clear(request, id):
     return redirect("/cart")
 
 
-
+@login_required(login_url="/user/login")
 def item_increment(request, id):
     cart = Cart(request)
     product = Product.objects.get(id=id)
@@ -60,7 +70,7 @@ def item_increment(request, id):
     return redirect("/cart")
 
 
-
+@login_required(login_url="/user/login")
 def item_decrement(request, id):
     cart = Cart(request)
     product = Product.objects.get(id=id)
@@ -68,13 +78,54 @@ def item_decrement(request, id):
     return redirect("/cart")
 
 
-
+@login_required(login_url="/user/login")
 def cart_clear(request):
     cart = Cart(request)
     cart.clear()
     return redirect("cart_detail")
 
 
-
+@login_required(login_url="/user/login")
 def cart_detail(request):
-    return render(request, 'cart/cart_detail.html')
+    return render(request, 'cart.html')
+
+@login_required(login_url="/user/login")
+def checkout(request):
+    if request.method=="POST":
+        phonenumber=request.POST.get('phonenumber')
+        email=request.POST.get('email')
+        address=request.POST.get('address')
+        payment=request.POST.get('payment')
+        cart=request.session.get('cart')
+        uid=request.session.get('_auth_user_id')
+        user=User.objects.get(pk=uid)
+        
+        for i in cart:
+            a=(float(cart[i]['price']))
+            b=cart[i]['quantity']
+            total=a*b
+            
+            order=Order(
+                user=user,
+                name=cart[i]['name'],
+                price=cart[i]['price'],
+                quantity=cart[i]['quantity'],
+                image=cart[i]['image'],
+                phonenumber=phonenumber,
+                email=email,
+                address=address,
+                payment=payment,
+                total=total,
+            )
+            order.save()
+        request.session['cart']={}
+        return redirect("/")
+
+       
+def order(request):
+    uid=request.session.get('_auth_user_id')
+    user=User.objects.get(pk=uid)
+    order=Order.objects.filter(user=user)
+
+    return render(request, "order.html",{'order':order})
+
